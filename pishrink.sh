292c804c7c6afd17510c98a4bce7fba07375fc87
paniy <louisowen@me.com>
2021-09-30 14:28:53 +0800

    Update pishrink.sh

--- pishrink.sh
+++ pishrink.sh
@@ -87,20 +87,47 @@ function set_autoexpand() {
     #####Do not touch the following lines#####
 cat <<\EOF1 > "$mountdir/etc/rc.local"
 #!/bin/bash
+# Put your custom commands here that should be executed once
+# the system init finished. By default this file does nothing.
+
 board=$(cat /tmp/sysinfo/board_name | cut -d , -f2)
-if [[ ! -f /tmp/panyi_${board} ]]; then
+if [[ ! -f /etc/panyi_${board} ]]; then
     DISK="/dev/mmcblk0"
     PART="/dev/mmcblk0p2"
-    fdisk -l ${DISK}>> /dev/null 2>&1 || (echo "could not find device $DISK - please check the name" && exit 1)
+    fdisk -l ${DISK}>> /dev/null 2>&1 || (echo "找不到磁盘: $DISK - 请测试磁盘名称！" >>/etc/panyi_${board} && exit 1)
     MAXSIZEMB=$(printf %s\\n 'unit MB print list' | parted | grep "Disk ${DISK}" | cut -d' ' -f3 | tr -d MB)
-    echo "[ok] ${PART} 将会扩展到 ${MAXSIZEMB} MB " >>/tmp/panyi_${board}
-    parted ${DISK} resizepart 2 ${MAXSIZEMB}
-    [ $? != 0 ] && echo "扩展分区出错了！" >>/tmp/panyi_${board}
-    e2fsck -y ${PART}
-    resize2fs ${PART}
-    [ $? = 0 ] && echo "文件系统扩展成功" >>/tmp/panyi_${board}
+    if [[ ${MAXSIZEMB} -gt 5120 ]]; then
+        echo "[ok] ${PART} 将会扩展到 5120 MB " >>/etc/panyi_${board}
+        parted ${DISK} resizepart 2 5120
+        [ $? != 0 ] && echo "扩展分区出错了！" >>/etc/panyi_${board}
+        e2fsck -y ${PART}
+        resize2fs ${PART}
+        [ $? = 0 ] && echo "文件系统扩展成功" >>/etc/panyi_${board}
+        parted ${DISK} mkpart primary ext2 5120 ${MAXSIZEMB}
+        [ $? != 0 ] && echo "新建分区出错了！" >>/etc/panyi_${board}
+        mkfs.ext4 /dev/mmcblk0p3
+        [ $? != 0 ] && echo "格式化新分区出错了！" >>/etc/panyi_${board}
+        mount /dev/mmcblk0p3 /panyi
+        if [ $? = 0 ]; then
+            echo "新分区完成！" >>/etc/panyi_${board}
+            dd if=/dev/zero of=/panyi/.swapfilep bs=1024 count=1048576
+            chown root:root /panyi/.swapfilep
+            chmod 0600 /panyi/.swapfilep
+            mkswap /panyi/.swapfilep
+            swapon /panyi/.swapfilep
+        fi
+    else
+        echo "[ok] ${PART} 将会扩展到 ${MAXSIZEMB} MB " >>/etc/panyi_${board}
+        parted ${DISK} resizepart 2 ${MAXSIZEMB}
+        [ $? != 0 ] && echo "扩展分区出错了！" >>/etc/panyi_${board}
+        e2fsck -y ${PART}
+        resize2fs ${PART}
+        [ $? = 0 ] && echo "文件系统扩展成功" >>/etc/panyi_${board}
+    fi
 fi
+
 exit 0
+
 EOF1
     #####End no touch zone#####
     chmod +x "$mountdir/etc/rc.local"
@@ -297,7 +324,7 @@ extra_space=$(($currentsize - $minsize))
 logVariables $LINENO extra_space
 for space in 5000 1000 100; do
   if [[ $extra_space -gt $space ]]; then
-    minsize=$(($minsize + $space + 32768))
+    minsize=$(($minsize + $space + 8192))
     break
   fi
 done
